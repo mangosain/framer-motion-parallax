1701116e29ce48db31497cb8f157f39b813bf951
Manav Gosain <mu@Manavs-MacBook-Pro.local>
2024-07-05 00:06:30 +0530

    parallax effect using framer-motion in nextJS

---
+++ components/text/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const TextPage = () => {
+  return (
+    <div className="bg-black text-white px-72 py-20 flex flex-col gap-24 font-thin text-2xl text-justify">
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio provident
+        voluptas cupiditate nisi obcaecati numquam ea reprehenderit blanditiis
+        error a doloribus porro, rem suscipit deserunt ipsum, aliquam quisquam
+        officia possimus accusamus ab? Praesentium aliquid id saepe nulla ab
+        laborum, ipsum velit earum cum soluta odit, possimus dolorum inventore
+        optio quasi.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+        velit? Placeat libero voluptatibus non alias veniam asperiores sapiente
+        deserunt. Fugiat quaerat, illo vitae repellat sed aut? Laudantium
+        aspernatur, accusantium officia incidunt harum corrupti quibusdam.
+        Maxime vel odit eligendi, adipisci libero, quia tempore mollitia amet
+        animi ad expedita dolorum omnis atque.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
+        quibusdam numquam repudiandae nostrum, ea placeat. Consequatur provident
+        vero ex, eveniet reiciendis quo ab iure fugiat error magni accusantium
+        ea incidunt optio ut cum earum explicabo facere! Ducimus dolore quos
+        mollitia sint, doloremque labore nihil inventore, consequatur voluptates
+        ad iure! Beatae.
+      </p>
+    </div>
+  );
+};
+
+export default TextPage;
